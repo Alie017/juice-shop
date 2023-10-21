70a854243dc0ae41072175039a1c067c4f71afb0
Ali Emre AYGÜN <81041852+Alie017@users.noreply.github.com>
2023-10-22 01:51:32 +0300

    Fix Authentication

--- routes/changePassword.ts
+++ routes/changePassword.ts
@@ -11,39 +11,42 @@ const security = require('../lib/insecurity')
 const cache = require('../data/datacache')
 const challenges = cache.challenges
 
-module.exports = function changePassword () {
-  return ({ query, headers, connection }: Request, res: Response, next: NextFunction) => {
-    const currentPassword = query.current
-    const newPassword = query.new
-    const newPasswordInString = newPassword?.toString()
-    const repeatPassword = query.repeat
-    if (!newPassword || newPassword === 'undefined') {
-      res.status(401).send(res.__('Password cannot be empty.'))
-    } else if (newPassword !== repeatPassword) {
-      res.status(401).send(res.__('New and repeated password do not match.'))
-    } else {
-      const token = headers.authorization ? headers.authorization.substr('Bearer='.length) : null
-      const loggedInUser = security.authenticatedUsers.get(token)
-      if (loggedInUser) {
-        if (currentPassword && security.hash(currentPassword) !== loggedInUser.data.password) {
-          res.status(401).send(res.__('Current password is not correct.'))
-        } else {
-          UserModel.findByPk(loggedInUser.data.id).then((user: UserModel | null) => {
-            if (user != null) {
-              user.update({ password: newPasswordInString }).then((user: UserModel) => {
-                challengeUtils.solveIf(challenges.changePasswordBenderChallenge, () => { return user.id === 3 && !currentPassword && user.password === security.hash('slurmCl4ssic') })
-                res.json({ user })
-              }).catch((error: Error) => {
-                next(error)
-              })
-            }
-          }).catch((error: Error) => {
-            next(error)
-          })
-        }
-      } else {
-        next(new Error('Blocked illegal activity by ' + connection.remoteAddress))
+const { User } = require('./user-model'); // Import your user model
+const bcrypt = require('bcrypt'); // Import a bcrypt library for password hashing
+
+module.exports = function changePassword() {
+  return async (req, res, next) => {
+    const { query, headers, user, connection } = req;
+    const currentPassword = query.current;
+    const newPassword = query.new;
+    const repeatPassword = query.repeat;
+
+    try {
+      // a more relevant solution to make the auhentication in a more secure way ( Hopefully :) )
+      if (!user) { 
+      return res.status(401).json({ message: 'Unauthorized. Please log in.' });
       }
+
+      const passwordMatches = await bcrypt.compare(currentPassword, user.password);
+      if (!passwordMatches) {
+        return res.status(401).json({ message: 'Current password is incorrect.' });
+      }
+      // Tried to use a simple mechanism to check whether the password of the user matches the current password
+
+      if (newPassword !== repeatPassword) {
+        return res.status(400).json({ message: 'New and repeated password do not match.' });
+      }
+
+      const hashedPassword = await bcrypt.hash(newPassword, 10); 
+      user.password = hashedPassword;
+      await user.save();
+      // I've tried to use hashing 
+
+      return res.status(200).json({ message: 'Password changed successfully.' });
+    } catch (error) {
+      
+      console.error(error);
+      return res.status(500).json({ message: 'An error occurred while changing the password.' });
     }
-  }
-}
+  };
+};
